eb96028721cb27dd15d6706c038d9c20ab262873
Qiqi Jin <ginqi7@gmail.com>
2025-07-13 14:40:41 +0800

    Init

---
+++ Casks/wechat@3.8.10.rb
@@ -0,0 +1,11 @@
+cask "wechat@3.8.10" do
+  version "3.8.10"
+  sha256 "10fe0e2e76cd513f2123cddfacaa68c39ffd6f305e5f6d746e6dc3ba6eaabf84"
+
+  url "https://dldir1v6.qq.com/weixin/mac/WeChatMac_10_15.dmg"
+  name "WeChat 3.8.10"
+  desc "WeChat 3.8.10"
+  homepage "https://mac.weixin.qq.com/"
+
+  app "WeChat.app"
+end
