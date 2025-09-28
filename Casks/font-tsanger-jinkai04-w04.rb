b1fc286f132f9cb7f2f239683a91c982602737de
Qiqi Jin <ginqi7@gmail.com>
2025-09-28 18:00:12 +0800

    A Chinese font: 仓耳今楷04-W04

---
+++ Casks/font-tsanger-jinkai04-w04.rb
@@ -0,0 +1,8 @@
+cask "font-tsanger-jinkai04-w04" do
+  version "1.0"
+  sha256 "7d5e78592796624ca690cc47a3bef5623277a5137d52df87b139f03d1022c9fa"
+  url "https://www.tsanger.cn/download/%E4%BB%93%E8%80%B3%E4%BB%8A%E6%A5%B704-W04.ttf"
+  name "TsangerJinKai04-W04"
+  desc "A Chinese font from Tsanger"
+  font "仓耳今楷04-W04.ttf"
+end
