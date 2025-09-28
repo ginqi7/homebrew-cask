73db90bda12647d58d31da6b26fe30b42798d5ff
Qiqi Jin <ginqi7@gmail.com>
2025-09-28 18:14:15 +0800

    A cask about the Supercharge Trial.

---
+++ Casks/supercharge-trial.rb
@@ -0,0 +1,11 @@
+cask "supercharge-trial" do
+  version "1.21.0"
+  sha256 "1a4abc82174c60e7b3c1f152c73b515d604cfbe6e9f7c4b77db44e272d1b2007"
+
+  url "https://www.dropbox.com/scl/fi/8dkra3j6gevn3ocx5ypoj/Supercharge-1.21.0-trial-1758661994.zip?rlkey=hh67ow8156ye8b76l9po3jb9m&raw=1"
+  name "Supercharge Trial"
+  desc "A trial version of Supercharge, a productivity app by Sindre Sorhus."
+  homepage "https://sindresorhus.com/supercharge"
+
+  app "Supercharge.app"
+end
