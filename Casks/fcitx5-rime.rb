112bc06a7bb75d9013739664d7a8f8beb06f67bd
Qiqi Jin <ginqi7@gmail.com>
2026-01-10 15:45:31 +0800

    Add fcitx5-rime

---
+++ Casks/fcitx5-rime.rb
@@ -0,0 +1,11 @@
+cask "fcitx5-rime" do
+  version "0.2.11"
+  sha256 "d0d0326962a51f2304851fb9a9289aaed0c67a94e893cb8bade3696ea0682267"
+
+  url "https://github.com/fcitx-contrib/fcitx5-macos-installer/releases/download/0.2.11/Fcitx5-Rime.zip"
+  name "Fcitx5 Rime macOS"
+  desc "Installer of Fcitx5 macOS "
+  homepage "https://github.com/fcitx-contrib/fcitx5-macos-installer"
+
+  app "Fcitx5Installer.app"
+end
