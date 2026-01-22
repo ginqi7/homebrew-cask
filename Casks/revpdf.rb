48237988d298cdecd8f9a8f62c366fb3d961b648
Qiqi Jin <ginqi7@gmail.com>
2026-01-22 21:44:13 +0800

    add cask: revpdf
    Add initial Homebrew cask formula for RevPDF (privacy-first PDF editor)

---
+++ Casks/revpdf.rb
@@ -0,0 +1,10 @@
+cask "revpdf" do
+  version "0.0.1"
+  sha256 "685208d85209c3c85e8a5ae1968e2f2c939e84d4dcbebb0254ff26503ff7993c"
+  url "https://github.com/Pawandeep-prog/revpdf-release/raw/refs/heads/main/macos/RevPDF_Editor_arm64.dmg"
+  name "RevPDF"
+  desc "Privacy-First PDF Editor"
+  homepage "https://revpdf.com/"
+
+  app "RevPDF Editor.app"
+end
