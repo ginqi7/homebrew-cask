73c4139fb5a17e3989db14e0d5f5e371b49e0cf0
Qiqi Jin <ginqi7@gmail.com>
2026-06-14 10:19:35 +0800

    Add iKeyMouse cask

---
+++ Casks/ikey-mouse.rb
@@ -0,0 +1,11 @@
+cask "ikey-mouse" do
+  version "0.2.11"
+  sha256 "d0d0326962a51f2304851fb9a9289aaed0c67a94e893cb8bade3696ea0682267"
+
+  url "https://ikeymouse.com/forum.php?mod=attachment&aid=MjA5fGU1NjFlMWMxfDE3ODE0MDMyNzJ8MHwz"
+  name "iKeyMouse"
+  desc "iKeyMouse"
+  homepage "https://ikeymouse.com/thread-3-1-1.html"
+
+  app "Ik mouse.app"
+end
