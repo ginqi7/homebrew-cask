49a2757394972f9b79b90655adb062e3c1a3afe9
Qiqi Jin <ginqi7@gmail.com>
2026-06-18 11:25:52 +0800

    Add Sideloadly cask to Homebrew formulae

---
+++ Casks/sideloadly.rb
@@ -0,0 +1,9 @@
+cask "sideloadly" do
+  version "v0.60.0"
+  sha256 "428d062af1ca819712fb12cb0ace25fa49c80d9735c73cda3cbaf09ffcd63212"
+  url "https://sideloadly.io/SideloadlySetup.dmg"
+  name "sideloadly"
+  desc "sideloadly"
+  homepage "https://sideloadly.io/"
+  app "Sideloadly.app"
+end
